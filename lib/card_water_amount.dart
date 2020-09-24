d814d37860c77c265e76125b53d5daf92ee543fe
Arthur Gonzaga <arthurgonzagaxyz@gmail.com>
2020-09-23 22:25:41 -0300

    Implement of the Water Icons

--- lib/card_water_amount.dart
+++ lib/card_water_amount.dart
@@ -18,10 +18,106 @@ class CardWaterState extends State<CardWater>{
       body: PageView(
         controller: widget.controller,
         children: [
-          Center(child: Text("100 ml")),
-          Center(child: Text("200 ml")),
-          Center(child: Text("300 ml")),
-        ],
+          Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    child: Image.asset("imgs/100ml.png"),
+                    width: 45,
+                    height: 45,
+                  ),
+                  Text(
+                    "100 ml",
+                    style: TextStyle(
+                        color: Color.fromRGBO(102, 180, 255, 1),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              )
+          ),
+          Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    child: Image.asset("imgs/150ml.png"),
+                    width: 45,
+                    height: 45,
+                  ),
+                  Text(
+                    "150 ml",
+                    style: TextStyle(
+                        color: Color.fromRGBO(102, 180, 255, 1),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              )
+          ),
+          Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    child: Image.asset("imgs/200ml.png"),
+                    width: 45,
+                    height: 45,
+                  ),
+                  Text(
+                      "200 ml",
+                      style: TextStyle(
+                          color: Color.fromRGBO(102, 180, 255, 1),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),
+            ),
+                ],
+              )
+          ),
+          Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    child: Image.asset("imgs/250ml.png"),
+                    width: 50,
+                    height: 50,
+                  ),
+                  Text(
+                    "250 ml",
+                    style: TextStyle(
+                        color: Color.fromRGBO(102, 180, 255, 1),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              )
+          ),
+          Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    child: Image.asset("imgs/250ml.png"),
+                    width: 50,
+                    height: 50,
+                  ),
+                  Text(
+                      "300 ml",
+                      style: TextStyle(
+                        color: Color.fromRGBO(102, 180, 255, 1),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),
+            ),
+                ],
+              ),
+          )],
       ),
     );
   }
