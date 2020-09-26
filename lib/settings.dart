3dfb3f6734b3cd1218ee0722312cca1d9525d006
Arthur Gonzaga <arthurgonzagaxyz@gmail.com>
2020-09-25 22:27:27 -0300

    Changes on the UI

--- lib/settings.dart
+++ lib/settings.dart
@@ -76,8 +76,12 @@ class SettingsState extends State<Settings>{
                       // Snapshot is list like: [height, weight, bmi, goal];
                       builder: (context, snapshot) {
                         if(snapshot.hasData){
-                          controllerWeight.text = snapshot.data[0].toString();
-                          controllerHeight.text = snapshot.data[1].toString();
+                          controllerHeight.text = snapshot.data[0].toString();
+                          controllerWeight.text = snapshot.data[1].toString();
+                          height = snapshot.data[1];
+                          weight = snapshot.data[2];
+                          goal = snapshot.data[3];
+                          bmi = snapshot.data[2];
                           return SafeArea(
                             child: Column(
                               children: [
@@ -111,6 +115,30 @@ class SettingsState extends State<Settings>{
                                     color: Colors.blue,
                                     child: Text("Save",style: TextStyle(color: Colors.white),),
                                   ),
+                                ),
+                                Center(
+                                  child: Container(
+                                    margin: EdgeInsets.all(32),
+                                    child: RichText(
+                                      text: TextSpan(
+                                        style: TextStyle(color: Colors.blue),
+                                        children: [
+                                          TextSpan(text:"You need to drink "),
+                                          TextSpan(text:"${goal.toStringAsFixed(1)} Liters\n\n", style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.blue,
+                                            fontSize: 21
+                                          )),
+                                          TextSpan(text:"Your BMI is "),
+                                          TextSpan(text:"${bmi.toStringAsFixed(2)}", style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.blue,
+                                              fontSize: 21
+                                          )),
+                                        ]
+                                      ),
+                                    ),
+                                  ),
                                 )
                               ],
                             ),
@@ -135,18 +163,17 @@ class SettingsState extends State<Settings>{
     bmi = (prefs.getDouble('bmi') ?? (weight/sqrt(height)));
     goal = (prefs.getDouble('goal') ?? 2.0);
 
-    //print([height, weight, bmi, goal]);
+    print([height, weight, bmi, goal]);
     return [height, weight, bmi, goal];
   }
 
   Future<void> saveData(BuildContext context) async{
     // Todo: fix some bugs here (update and round the numbers)
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setDouble("height", double.parse(controllerHeight.text));
-    prefs.setDouble("weight", double.parse(controllerWeight.text));
-    prefs.setDouble("bmi",
-        double.parse(controllerWeight.text)/sqrt(double.parse(controllerHeight.text)));
-    prefs.setDouble("goal", 0.035 * double.parse(controllerWeight.text));
+    prefs.setDouble("weight", weight);
+    prefs.setDouble("height", height);
+    prefs.setDouble("bmi", bmi);
+    prefs.setDouble("goal", goal);
 
     Scaffold.of(context).showSnackBar(
       SnackBar(content: Text("Saved Successfully"))
